8a62b9369a86be0e4ca2ba475bb61b551b59676e
erde123 <108157433+erde123@users.noreply.github.com>
2025-09-29 11:36:44 +0200

    first commit

---
+++ src/main/java/nl/fontys/tweetleapigateway/TweetleApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.tweetleapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetleApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweetleApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/fontys/tweetleapigateway/TweetleApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.tweetleapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetleApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
