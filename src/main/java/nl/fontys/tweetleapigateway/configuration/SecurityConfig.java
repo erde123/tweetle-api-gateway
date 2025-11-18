5ee6455b56d41cedb8331123ec5b9c04f9b91a6d
erde123 <108157433+erde123@users.noreply.github.com>
2025-11-18 21:17:30 +0100

    attempt to fix swagger (failed)

---
+++ src/main/java/nl/fontys/tweetleapigateway/configuration/SecurityConfig.java
@@ -0,0 +1,29 @@
+package nl.fontys.tweetleapigateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/webjars/**",
+                                "/api/users/v3/api-docs"
+                        ).permitAll()
+                        .anyExchange().permitAll()
+                )
+                .build();
+    }
+}
